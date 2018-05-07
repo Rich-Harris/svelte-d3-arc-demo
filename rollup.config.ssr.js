b4e2b7fbd9adcd927815eaaa13405970785e1ffb
Chris Knox <chris.knox@nzherald.co.nz>
2018-05-07 12:26:13 +1200

    update to svelte v2

--- rollup.config.ssr.js
+++ rollup.config.ssr.js
@@ -3,9 +3,11 @@ import json from 'rollup-plugin-json';
 import svelte from 'rollup-plugin-svelte';
 
 export default {
-	entry: 'src/Viz.html',
-	dest: 'build/Viz-ssr.js',
-	format: 'cjs',
+	input: 'src/Viz.html',
+	output: {
+		file: 'build/Viz-ssr.js',
+		format: 'cjs'
+	},
 	plugins: [
 		resolve(),
 		json(),
